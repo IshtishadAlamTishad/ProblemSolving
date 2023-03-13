e85e786f0db83eb0b893e012987e33fe1bba8edb
Tishad <96460346+IshtishadAlamTishad@users.noreply.github.com>
2023-03-14 00:04:03 +0600

    Create Average2.java

---
+++ BeeCrowd/Average2.java
@@ -0,0 +1,18 @@
+// Author : Tishad
+
+import java.util.Scanner;
+public class Average2
+{
+ public static void main(String[] args) {
+   
+    Scanner sc = new Scanner(System.in);
+    double a,b,c;
+    a = sc.nextDouble();
+    b = sc.nextDouble();
+    c = sc.nextDouble();
+
+    double s = (a*2)+(b*3)+(c*5);
+    System.out.println(s/(10));
+ }
+}
+    
