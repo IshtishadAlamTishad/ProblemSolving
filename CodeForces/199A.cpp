0fa7a568f894e711eecb637e84c7db12ebfe5e6b
Tishad <96460346+IshtishadAlamTishad@users.noreply.github.com>
2023-05-27 13:29:56 +0600

    Create 199A.cpp

---
+++ CodeForces/199A.cpp
@@ -0,0 +1,25 @@
+
+/*
+
+Author : Tishad07
+
+Time   : 5/27/2023 1:26:33 PM +06 GMT
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+
+int32_t main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0),cout.tie(0);
+	
+	int n;
+	cin >> n;
+	cout << 0 << ' ' << 0 << ' ' << n;	
+	
+	
+}
