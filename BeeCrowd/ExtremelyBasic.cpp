e9dfe4ea1ae3c19241e460aa40c521eaa1c7d48e
Tishad <96460346+IshtishadAlamTishad@users.noreply.github.com>
2023-05-31 02:16:50 +0600

    Create ExtremelyBasic.cpp

---
+++ BeeCrowd/ExtremelyBasic.cpp
@@ -0,0 +1,21 @@
+/*
+Author : Tishad07
+Time   : 5/31/2023 2:15:52 AM +06 GMT
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const int N = 2e3;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0),cout.tie(0);
+  
+  int x,y;
+  cin >> x >> y;
+  cout << "X = " << x+y << '\n';
+  
+}
