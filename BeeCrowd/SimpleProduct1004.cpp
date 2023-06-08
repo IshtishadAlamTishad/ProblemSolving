a0bd89e536b2493822b67329dbdc23b193156bba
Tishad <96460346+IshtishadAlamTishad@users.noreply.github.com>
2023-06-09 01:23:39 +0600

    Create SimpleProduct1004.cpp

---
+++ BeeCrowd/SimpleProduct1004.cpp
@@ -0,0 +1,22 @@
+
+/*
+Author : Tishad07
+Time   : 6/9/2023 1:17:23 AM +06 GMT
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const int N = 2e3;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0),cout.tie(0);
+  
+  int x,y;
+  cin >> x >> y;
+  cout << "PROD = " << (x*y) << '\n';
+
+}
