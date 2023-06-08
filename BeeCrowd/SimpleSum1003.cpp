bd99f4041e9ea180ebb7379cedfd2fee19f8b1bd
Tishad <96460346+IshtishadAlamTishad@users.noreply.github.com>
2023-06-09 01:22:31 +0600

    Create SimpleSum1003.cpp

---
+++ BeeCrowd/SimpleSum1003.cpp
@@ -0,0 +1,21 @@
+/*
+Author : Tishad07
+Time   : 6/9/2023 1:17:23 AM +06 GMT
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const int N = 2e3;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0),cout.tie(0);
+  
+  int x,y;
+  cin >> x >> y;
+  cout << "SOMA = " << (x+y) << '\n';
+
+}
