5b565d487320d6704fa64103913794a1aef8464d
Tishad <96460346+IshtishadAlamTishad@users.noreply.github.com>
2023-06-19 21:50:42 +0600

    Create HelpAThief.cpp

---
+++ GeeksForGeeks/HelpAThief.cpp
@@ -0,0 +1,28 @@
+bool cmp(pair<int,int> a,pair<int,int>b)
+{
+    return a.second > b.second;
+}
+
+class Solution {
+  public:
+    int maxCoins(int A[], int B[], int T, int N) {        
+        vector<pair<int,int> >v;
+        for(int i=0;i<N;i++) {
+            v.push_back(make_pair(A[i],B[i]));
+        }
+        sort(v.begin(),v.end(),cmp);
+        int sum=0;
+        auto it = v.begin();
+        while(it!=v.end() and T>0) {
+            if(T>=(*it).first) {
+                sum+= (*it).first * (*it).second;
+                T-= (*it).first;
+            } else {
+                sum+= (T*(*it).second);
+                T=0;
+            }
+            it++;
+        }
+        return sum;
+    }
+};
