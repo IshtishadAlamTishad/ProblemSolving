72a9be0351fcec0130eb790967cf8c9d7b75d632
Tishad <96460346+IshtishadAlamTishad@users.noreply.github.com>
2023-06-19 21:39:01 +0600

    Update and rename Check_If_two_arrays_are_equall.cpp to Check_If_two_arrays_are_equal_or_not.cpp

--- GeeksForGeeks/Check_If_two_arrays_are_equal_or_not.cpp
+++ GeeksForGeeks/Check_If_two_arrays_are_equal_or_not.cpp
@@ -1,9 +1,5 @@
-//User function template for C++
-
 class Solution{
     public:
-
-    //Function to check if two arrays are equal or not.
     bool check(vector<ll> A, vector<ll> B, int N) {
         sort(A.begin(),A.end());
         sort(B.begin(),B.end());
