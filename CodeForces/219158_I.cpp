cee9046d74aef5f9779d06c88fa5a499e5ae13b2
Tishad <96460346+IshtishadAlamTishad@users.noreply.github.com>
2023-07-28 22:42:57 +0600

    Create 219158_I.cpp

---
+++ CodeForces/219158_I.cpp
@@ -0,0 +1,28 @@
+
+/*
+Author : Tishad07
+Time   : 7/28/2023 10:41:19 PM +06
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const int N = 2e3;
+
+void solve()
+{
+    int a,b;
+    cin >> a >> b;
+    cout << ((a>=b) ? "Yes" : "No");
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0),cout.tie(0);
+    
+    solve();   
+    
+    
+}
